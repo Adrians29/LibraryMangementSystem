0eaf4181e8e3c44a3e1c304eccdd0fb50123d720
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 10:44:08 -0400

    Add constructor to Book

--- src/main/java/org/adrianegl/library/domain/Book.java
+++ src/main/java/org/adrianegl/library/domain/Book.java
@@ -1,7 +1,22 @@
 package org.adrianegl.library.domain;
 
+import org.adrianegl.library.util.Validation;
+
 public class Book extends Item {
     private String isbn;
     private String author;
     private String genre;
+
+
+    public Book(String title, ItemStatus status, String isbn, String author, String genre) {
+        super(title, status);
+
+        if (!Validation.isValidISBM(isbn)) {
+            throw new IllegalArgumentException("Invalid ISBN");
+        }
+
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
 }
