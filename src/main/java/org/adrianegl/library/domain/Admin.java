177e3978ebdd2d63f983f96d1cffd8f9fa4e51da
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 11:27:48 -0400

    Add constructor and methods to Admin

--- src/main/java/org/adrianegl/library/domain/Admin.java
+++ src/main/java/org/adrianegl/library/domain/Admin.java
@@ -1,4 +1,18 @@
 package org.adrianegl.library.domain;
 
 public class Admin extends User {
+    public Admin (String name) {
+        super(name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean canBorrow(Item item) {
+        return false;
+    }
+
 }
