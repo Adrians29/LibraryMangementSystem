dbb82df0f51b8de1318f837fa61167cf10547709
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 11:31:10 -0400

    Add fields to Library

--- src/main/java/org/adrianegl/library/domain/Library.java
+++ src/main/java/org/adrianegl/library/domain/Library.java
@@ -1,4 +1,11 @@
 package org.adrianegl.library.domain;
 
+import java.util.*;
+
 public class Library {
+    private List<Item> items;
+    private Map<String, User> users;
+    private Queue<User> waitingQueue;
+    private Stack<String> transactionHistory;
+    private Set<String> uniqueTitles;
 }
