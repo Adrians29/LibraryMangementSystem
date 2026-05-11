28d2316d0b0abf746752d4828d7b47e91bf18893
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 11:26:36 -0400

    Add constructor and methods to Teacher

--- src/main/java/org/adrianegl/library/domain/Teacher.java
+++ src/main/java/org/adrianegl/library/domain/Teacher.java
@@ -1,4 +1,19 @@
 package org.adrianegl.library.domain;
 
+import org.adrianegl.library.util.Constants;
+
 public class Teacher extends User {
+    public Teacher (String name) {
+        super(name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return Constants.MAX_ITEMS_TEACHER;
+    }
+
+    @Override
+    public boolean canBorrow(Item item) {
+        return borrowedItems.size() < getBorrowLimit();
+    }
 }
