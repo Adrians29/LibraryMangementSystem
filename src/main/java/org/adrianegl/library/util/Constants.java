5653a4ca7fc1ef6e0098fabf8c68d9d2e8270361
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 19:15:43 -0400

    Convert id into 4 different variables

--- src/main/java/org/adrianegl/library/util/Constants.java
+++ src/main/java/org/adrianegl/library/util/Constants.java
@@ -5,5 +5,8 @@ public class Constants {
     public static final int MAX_ITEMS_TEACHER = 10;
     public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
     public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
-    public static int nextId = 1;
+    public static int nextItemId = 1;
+    public static int nextStudentId = 1;
+    public static int nextTeacherId = 1;
+    public static int nextAdminId = 1;
 }
