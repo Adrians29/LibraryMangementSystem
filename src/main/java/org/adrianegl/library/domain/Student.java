5cdd91ac3f7c713155ad1d4b53bf31407f7f871b
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 11:01:08 -0400

    Add constructor and methods to Student

--- src/main/java/org/adrianegl/library/domain/Student.java
+++ src/main/java/org/adrianegl/library/domain/Student.java
@@ -1,4 +1,17 @@
 package org.adrianegl.library.domain;
 
 public class Student extends User {
+    public Student(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean canBorrow(Item item) {
+        return false;
+    }
 }
