50a8058dcb1dd79e01ba478cc7b6c35650abd380
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 10:22:16 -0400

    Add constant nextId

--- src/main/java/org/adrianegl/library/util/Constants.java
+++ src/main/java/org/adrianegl/library/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int MAX_ITEMS_TEACHER = 10;
     public static final String BOOKS_CSV_PATH = "src/main/resources/books.csv";
     public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
+    public static int nextId = 1;
 }
