8d974e1b7cfabc48deb7a2e48982cfb49dbd553a
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 10:13:37 -0400

    Add fields to User class

--- src/main/java/org/adrianegl/library/domain/User.java
+++ src/main/java/org/adrianegl/library/domain/User.java
@@ -1,4 +1,11 @@
 package org.adrianegl.library.domain;
 
-public class User {
+import java.util.List;
+
+public abstract class User {
+    protected String id;
+    protected String name;
+    protected List<Item> borrowedItems;
+
+
 }
