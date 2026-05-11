52891d83dd9fe67c0cd99f796df47ca5fc568dc3
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 17:13:13 -0400

    implement Reportable to item

--- src/main/java/org/adrianegl/library/domain/Item.java
+++ src/main/java/org/adrianegl/library/domain/Item.java
@@ -3,22 +3,34 @@ package org.adrianegl.library.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.adrianegl.library.interfaces.Reportable;
 import org.adrianegl.library.util.Constants;
 
 @Getter
 @ToString
-public class Item {
+public class Item implements Reportable {
     protected String id;
     protected String title;
     @Setter protected ItemStatus status;
 
-    public enum ItemStatus {
-        BORROWED, IN_STORE, LOST
-    }
 
+
+    public enum ItemStatus {
+        BORROWED, IN_STORE, LOST;
+    }
     public Item(String title, ItemStatus status) {
         this.id = String.format("%04d", Constants.nextId++);
         this.title = title;
             this.status = ItemStatus.IN_STORE;
+    }
+
+    @Override
+    public String getReportDetails() {
+        return "ID: "
+                    + id
+                    + "| Title: "
+                    + title
+                    + "| Status: "
+                    + status;
     }
 }
