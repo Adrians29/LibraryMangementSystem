623c0d37dded2812f2be1f1a799e3682150e1669
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 22:36:40 -0400

    Create Items

--- src/main/java/org/adrianegl/Main.java
+++ src/main/java/org/adrianegl/Main.java
@@ -1,9 +1,6 @@
 package org.adrianegl;
 
-import org.adrianegl.library.domain.Admin;
-import org.adrianegl.library.domain.Library;
-import org.adrianegl.library.domain.Student;
-import org.adrianegl.library.domain.Teacher;
+import org.adrianegl.library.domain.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +16,11 @@ public class Main {
         library.registerUser(student);
         library.registerUser(teacher);
         library.registerUser(admin);
+
+        Book book1 = new Book("Python for Beginners", Item.ItemStatus.IN_STORE, "1234567890123", "Kevin Bruce", "Education");
+        Book book2 = new Book("Project Hail Mary", Item.ItemStatus.IN_STORE, "2345678909876", "Andy Weir", "Fiction");
+        DVD dvd = new DVD("Batman Begins", Item.ItemStatus.IN_STORE, "Christopher Nolan", 140);
+        Magazine magazine = new Magazine("National Geographic", Item.ItemStatus.IN_STORE, "Polo", 9);
+
     }
 }
