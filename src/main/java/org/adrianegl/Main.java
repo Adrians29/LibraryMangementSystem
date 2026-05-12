524544212fc03ce404946c17294e2234e90d8fd1
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 22:26:38 -0400

    load users and items

--- src/main/java/org/adrianegl/Main.java
+++ src/main/java/org/adrianegl/Main.java
@@ -5,5 +5,8 @@ import org.adrianegl.library.domain.Library;
 public class Main {
     public static void main(String[] args) {
         Library library = new Library();
+
+        library.loadUsers();
+        library.loadItems();
     }
 }
