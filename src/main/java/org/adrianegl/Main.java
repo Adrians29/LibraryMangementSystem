5a5e6abcf28f60d1f6963d6e22439b25a91b46d9
Adrians29 <adrian.egl@hotmail.com>
2026-05-11 22:25:54 -0400

    Create a library on Main

--- src/main/java/org/adrianegl/Main.java
+++ src/main/java/org/adrianegl/Main.java
@@ -1,6 +1,9 @@
 package org.adrianegl;
 
+import org.adrianegl.library.domain.Library;
+
 public class Main {
     public static void main(String[] args) {
+        Library library = new Library();
     }
 }
